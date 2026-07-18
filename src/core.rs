5a349ddda9f00be6074a995d9664c9f949a040b5
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-07-18 22:33:52 +0300

    improve error handling

--- src/core.rs
+++ src/core.rs
@@ -3,11 +3,13 @@ use nom::{
     branch::alt,
     bytes::complete::{tag, take_until},
     character::complete::multispace0,
-    combinator::value,
+    combinator::{cut, value},
+    error::context,
     multi::separated_list0,
     number::complete::float,
     sequence::{delimited, pair, separated_pair},
 };
+use nom_language::error::{VerboseError, convert_error};
 
 #[derive(Debug)]
 pub enum JsonValue {
@@ -19,20 +21,39 @@ pub enum JsonValue {
     Object(Vec<(String, JsonValue)>),
 }
 
-pub fn parse_null(input: &str) -> IResult<&str, JsonValue> {
+pub fn parse(input: &str) -> Result<JsonValue, String> {
+    match alt((
+        parse_null,
+        parse_bool,
+        parse_number,
+        parse_json_string,
+        parse_array,
+        parse_object,
+    ))
+    .parse(input)
+    {
+        Ok((_, val)) => Ok(val),
+        Err(err) => match err {
+            nom::Err::Error(e) | nom::Err::Failure(e) => Err(convert_error(input, e)),
+            nom::Err::Incomplete(needed) => Err(format!("INCOMPLETE : {needed:#?}")),
+        },
+    }
+}
+
+fn parse_null(input: &str) -> IResult<&str, JsonValue, VerboseError<&str>> {
     let target = tag("null");
     let mut delim = delimited(multispace0, target, multispace0);
     delim.parse(input).map(|(rest, _)| (rest, JsonValue::Null))
 }
 
-pub fn parse_number(input: &str) -> IResult<&str, JsonValue> {
+fn parse_number(input: &str) -> IResult<&str, JsonValue, VerboseError<&str>> {
     let mut delim = delimited(multispace0, float, multispace0);
     delim
         .parse(input)
         .map(|(rest, num)| (rest, JsonValue::Number(num)))
 }
 
-pub fn parse_bool(input: &str) -> IResult<&str, JsonValue> {
+fn parse_bool(input: &str) -> IResult<&str, JsonValue, VerboseError<&str>> {
     let target = alt((value(true, tag("true")), value(false, tag("false"))));
     let mut delim = delimited(multispace0, target, multispace0);
     delim
@@ -40,48 +61,53 @@ pub fn parse_bool(input: &str) -> IResult<&str, JsonValue> {
         .map(|(rest, b)| (rest, JsonValue::Bool(b)))
 }
 
-pub fn parse_key_string(input: &str) -> IResult<&str, String> {
+fn parse_key_string(input: &str) -> IResult<&str, String, VerboseError<&str>> {
     let mut open = pair(multispace0, tag("\""));
-    let mut close = pair(tag("\""), multispace0);
+    let close = pair(tag("\""), multispace0);
     let (rest, _) = open.parse(input)?;
-    let (rest, result) = take_until("\"").parse(rest)?;
-    let (rest, _) = close.parse(rest)?;
+    let (rest, result) = cut(take_until("\"")).parse(rest)?;
+    let (rest, _) = cut(close).parse(rest)?;
     Ok((rest, result.to_string()))
 }
 
-pub fn parse_json_string(input: &str) -> IResult<&str, JsonValue> {
+fn parse_json_string(input: &str) -> IResult<&str, JsonValue, VerboseError<&str>> {
     parse_key_string(input).map(|(rest, s)| (rest, JsonValue::String(s)))
 }
 
 // let separator = delimited(tag(" "), tag(","), tag(" "));
-pub fn parse_array(input: &str) -> IResult<&str, JsonValue> {
+fn parse_array(input: &str) -> IResult<&str, JsonValue, VerboseError<&str>> {
     let open = delimited(multispace0, tag("["), multispace0);
     let close = delimited(multispace0, tag("]"), multispace0);
     let delim = delimited(multispace0, tag(","), multispace0);
-    let (rest, arr) =
-        delimited(open, separated_list0(delim, parse_json_value), close).parse(input)?;
+    let (rest, arr) = delimited(
+        open,
+        cut(separated_list0(delim, parse_json_value)),
+        cut(close),
+    )
+    .parse(input)?;
     Ok((rest, JsonValue::Array(arr)))
 }
 
-pub fn parse_object(input: &str) -> IResult<&str, JsonValue> {
+fn parse_object(input: &str) -> IResult<&str, JsonValue, VerboseError<&str>> {
     let open = delimited(multispace0, tag("{"), multispace0);
     let close = delimited(multispace0, tag("}"), multispace0);
     let colon = delimited(multispace0, tag(":"), multispace0);
     let delim = delimited(multispace0, tag(","), multispace0);
     let pair_parser = separated_pair(parse_key_string, colon, parse_json_value);
 
-    let (rest, arr) = delimited(open, separated_list0(delim, pair_parser), close).parse(input)?;
+    let (rest, arr) =
+        delimited(open, cut(separated_list0(delim, pair_parser)), cut(close)).parse(input)?;
     Ok((rest, JsonValue::Object(arr)))
 }
 
-pub fn parse_json_value(input: &str) -> IResult<&str, JsonValue> {
+fn parse_json_value(input: &str) -> IResult<&str, JsonValue, VerboseError<&str>> {
     let res = alt((
-        parse_null,
-        parse_bool,
-        parse_number,
-        parse_json_string,
-        parse_array,
-        parse_object,
+        context("null", parse_null),
+        context("bool", parse_bool),
+        context("number", parse_number),
+        context("string", parse_json_string),
+        context("array", parse_array),
+        context("object", parse_object),
     ))
     .parse(input)?;
     Ok(res)
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 pub mod core;
 
 fn main() {
-    let i = core::parse_null("null hello null").unwrap();
-    let j = core::parse_number("77.2002 hello float").unwrap();
-    let k = core::parse_bool("false hello bool").unwrap();
-    let l = core::parse_json_string("\"hello string\" hello bool").unwrap();
     let data = r#"
     [
         null,
@@ -24,13 +20,13 @@ fn main() {
             "langs": ["javascript","typescript","rust"]
         }
     ]"#;
-    let ob = r#"{"name":"mahmoud","age":26}"#;
-    let o = core::parse_object(ob).unwrap();
-    let arr = core::parse_array(data).unwrap();
-    dbg!(i);
-    dbg!(j);
-    dbg!(k);
-    dbg!(l);
-    dbg!(o);
-    dbg!(arr);
+    let ob = r#"
+        {
+            "name":"mahmoud",
+            "age":26
+        }"#;
+    let object = core::parse(ob);
+    let array = core::parse(data);
+    dbg!(object);
+    dbg!(array);
 }
